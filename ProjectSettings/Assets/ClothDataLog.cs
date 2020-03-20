7516abcbee2726eede5a5559f3dd8bf65dfcdcbc
aswinigh <aswighosh@gmail.com>
2020-03-20 19:24:31 +0530

    Log cloth data

---
+++ ProjectSettings/Assets/ClothDataLog.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothDataLog : MonoBehaviour {
+    Cloth clothMesh;
+    public Mesh bakedMesh;
+    public Vector3[] vertices;
+
+     void Start() {
+         clothMesh = this.GetComponent<Cloth>();
+        //clothMesh.BakeMesh(bakedMesh);
+        //vertices = bakedMesh.vertices;
+        //Debug.Log(vertices[2]);
+    }
+
+    void Update() {
+        Debug.Log("Time:"+Time.time);
+        vertices = clothMesh.vertices;
+        //vertices = bakedMesh.vertices;
+        Debug.Log(vertices[1]);
+        // foreach (Vector3 v in vertices)
+        // {   
+        //     Debug.Log(v); 
+        // }
+       
+    }
+    
+}
+    
---
+++ ProjectSettings/Assets/ClothVertices.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothVertices : MonoBehaviour
+{   public float offset;
+    public Mesh clothMesh;
+    // Start is called before the first frame update
+    public Vector3[] vertices;
+    void Start()
+    {
+        vertices = clothMesh.vertices;
+
+        for(int i=0;i<vertices.Length;i++)
+        {
+            vertices[i].x += offset;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
